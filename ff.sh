07e07d7b064d5da877026226acb8f55ef93bb172
Vincent Brenet <vbrenet@gmail.com>
2017-09-22 17:05:51 +0200

    shell script tested to launch anonymous APEX execution

--- ff.sh
+++ ff.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/bash
-echo scriptname : $0
-echo nb param $#
-#echo $1
 if [[ ($# -lt 2) || ($# -gt 4) ]];
 then
 echo "Usage : ff <orgalias> <SObjectName> [nb analysed records - default : all] [nb records by batch unit - default : 2000]"
@@ -21,12 +18,13 @@ batchSz=2000
 else
 batchSz=$4
 fi
-echo P1 : $1
-echo P2 : $2
+echo Org : $1
+echo Object : $2
 echo nbRec : $nbRec
 echo batchSz : $batchSz
-echo "fieldAnalysisBatch fab = new fieldAnalysisBatch('$2',$nbRec);"
-echo "Id theId = Database.ExecuteBatch(fab,$batchSz);"
+echo "fieldAnalysisBatch fab = new fieldAnalysisBatch('$2',$nbRec);"> myexec
+echo "Id theId = Database.ExecuteBatch(fab,$batchSz);" >> myexec
+sfdx force:apex:execute -u $1 -f myexec
 
 
 
